bf5a5a8ab7e8246491fdc283e0880501d880d37e
Thaís Souza <thais_de_souza@hotmail.com>
2020-10-17 02:56:29 -0300

    Inserindo imagens no mapa

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -22,6 +22,8 @@ export default {
     },
 
     async create(request: Request, response: Response){
+        console.log(request.files);
+
         const {
             name,
             latitude,
@@ -34,6 +36,11 @@ export default {
     
         const orphanagesRepository = getRepository(Orphanage);
     
+        const requestImages = request.files as Express.Multer.File[];
+        const images = requestImages.map(image => {
+            return { path: image.filename }
+        })
+
         const orphanage = orphanagesRepository.create({
             name,
             latitude,
@@ -42,6 +49,7 @@ export default {
             instructions,
             opening_hours,
             open_on_weekends,
+            images
         });
     
         await orphanagesRepository.save(orphanage);
